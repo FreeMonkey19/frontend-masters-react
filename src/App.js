bfb7c12d09b20eaec537ac5b4689d969bf2fb4d5
luckySeattleMonkey <freeMonkey19@gmail.com>
2020-07-04 19:35:52 -0700

    build component SearchParams

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
-
+import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
-      <Pet name="Ginger" animal="Dog" breed="Springer Spaniel" />
+      <SearchParams />
     </div>
   );
 };
